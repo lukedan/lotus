7e443c4ccc7e7741f276aab3f34c72a320d1683d
Xuanyi Zhou <zxy19980101@sina.cn>
2021-04-20 21:30:52 -0400

    refactor stack allocator

--- include/pbd/utils/stack_allocator.h
+++ include/pbd/utils/stack_allocator.h
@@ -22,12 +22,7 @@ namespace pbd {
 			/// Creates an empty object.
 			scoped_bookmark() = default;
 			/// Creates a new bookmark object.
-			[[nodiscard]] inline static scoped_bookmark create(stack_allocator &alloc = for_this_thread()) {
-				scoped_bookmark result;
-				result._alloc = &alloc;
-				alloc.set_bookmark();
-				return result;
-			}
+			[[nodiscard]] static scoped_bookmark create(stack_allocator& = for_this_thread());
 			/// Move construction.
 			scoped_bookmark(scoped_bookmark &&src) : _alloc(std::exchange(src._alloc, nullptr)) {
 			}
@@ -88,30 +83,10 @@ namespace pbd {
 		/// No copy assignment.
 		stack_allocator &operator=(const stack_allocator&) = delete;
 		/// Frees all pages.
-		~stack_allocator() {
-			assert(_top_bookmark == nullptr);
-			free_unused_pages();
-			while (_top_page) {
-				_page_ref next = _top_page.header->previous;
-				auto free_func = _top_page.header->free_page;
-				free_func(_top_page.memory);
-				_top_page = next;
-			}
-		}
+		~stack_allocator();
 
 		/// Allocates a new block of memory.
-		void *allocate(std::size_t size, std::size_t align) {
-			if (void *result = _top_page.allocate(size, align)) {
-				return result;
-			}
-			_take_page();
-			if (void *result = _top_page.allocate(size, align)) {
-				return result;
-			}
-			_return_page();
-			_top_page = _allocate_new_page(_top_page, page_size + size);
-			return _top_page.allocate(size, align);
-		}
+		void *allocate(std::size_t size, std::size_t align);
 		/// Allocates a new block of memory. This function does not initialize the memory.
 		template <typename T> T *allocate() {
 			return static_cast<T*>(allocate(sizeof(T), alignof(T)));
@@ -124,26 +99,10 @@ namespace pbd {
 		}
 		/// Resets the allocator to the state before the last bookmark was allocated. All allocated memory since then
 		/// must be properly freed by this point.
-		void pop_bookmark() {
-			assert(_top_bookmark);
-			_bookmark mark = *_top_bookmark;
-			_top_bookmark->~_bookmark();
-			_top_bookmark = mark.previous;
-			while (_top_page.memory != mark.page) {
-				_return_page();
-			}
-			_top_page.current = mark.current;
-		}
+		void pop_bookmark();
 
 		/// Frees all pages in \ref _free_pages.
-		void free_unused_pages() {
-			while (_free_pages) {
-				_page_ref next = _free_pages.header->previous;
-				auto free_func = _free_pages.header->free_page;
-				free_func(_free_pages.memory);
-				_free_pages = next;
-			}
-		}
+		void free_unused_pages();
 
 		/// Returns the \ref stack_allocator for this thread.
 		static stack_allocator &for_this_thread();
@@ -162,24 +121,11 @@ namespace pbd {
 			_page_ref(std::nullptr_t) : memory(nullptr), header(nullptr), current(nullptr), end(nullptr) {
 			}
 			/// Creates a new reference to the given newly allocated page. \ref header is not initialized.
-			[[nodiscard]] inline static _page_ref to_new_page(void *ptr, std::size_t sz) {
-				_page_ref result = uninitialized;
-				result.memory = ptr;
-				result.current = ptr;
-				result.end = static_cast<std::byte*>(ptr) + sz;
-				return result;
-			}
+			[[nodiscard]] static _page_ref to_new_page(void*, std::size_t sz);
 
 			/// Allocates a block of memory from this page. If there's not enough space within this page, this
 			/// function returns \p nullptr. The returned memory block is not initialized.
-			[[nodiscard]] void *allocate(std::size_t size, std::size_t align) {
-				std::size_t sz = static_cast<std::byte*>(end) - static_cast<std::byte*>(current);
-				if (void *result = std::align(align, size, current, sz)) {
-					current = static_cast<std::byte*>(current) + size;
-					return result;
-				}
-				return nullptr;
-			}
+			[[nodiscard]] void *allocate(std::size_t size, std::size_t align);
 			/// \overload
 			template <typename T> [[nodiscard]] T *allocate() {
 				return static_cast<T*>(allocate(sizeof(T), alignof(T)));
@@ -208,12 +154,7 @@ namespace pbd {
 			_page_header(uninitialized_t) {
 			}
 			/// Creates a header object with the given reference to the previous page.
-			[[nodiscard]] inline static _page_header create(_page_ref prev, void (*free)(void*)) {
-				_page_header result = uninitialized;
-				result.previous = prev;
-				result.free_page = free;
-				return result;
-			}
+			[[nodiscard]] static _page_header create(_page_ref prev, void (*free)(void*));
 
 			_page_ref previous = uninitialized; ///< The previous page.
 			void (*free_page)(void*); ///< The function that should be used to free this page.
@@ -224,13 +165,7 @@ namespace pbd {
 			_bookmark(uninitialized_t) {
 			}
 			/// Creates a new bookmark object.
-			[[nodiscard]] inline static _bookmark create(void *page, void *cur, _bookmark *prev) {
-				_bookmark result = uninitialized;
-				result.page = page;
-				result.current = cur;
-				result.previous = prev;
-				return result;
-			}
+			[[nodiscard]] static _bookmark create(void *page, void *cur, _bookmark *prev);
 
 			void *page; ///< Address of the page that this bookmark is in.
 			void *current; ///< Position of the bookmark within the page.
@@ -250,23 +185,9 @@ namespace pbd {
 
 		/// Replaces \ref _top_page with a new page. If \ref _free_pages is empty, a page is taken from the list;
 		/// otherwise a new page is allocated.
-		void _take_page() {
-			if (_free_pages) {
-				_page_ref page = _free_pages;
-				_free_pages = _free_pages.header->previous;
-				page.header->previous = _top_page;
-				_top_page = page;
-			} else {
-				_top_page = _allocate_new_page(_top_page);
-			}
-		}
+		void _take_page();
 		/// Assumes that \ref _top_page is empty and returns it to \ref _free_pages.
-		void _return_page() {
-			_page_ref new_top = _top_page.header->previous;
-			_top_page.reset(_page_header::create(_free_pages, _top_page.header->free_page));
-			_free_pages = _top_page;
-			_top_page = new_top;
-		}
+		void _return_page();
 
 		_page_ref _top_page = nullptr; ///< The page currently in use.
 		/// A list of free pages. All pages in this list have correct \ref _page_ref::current fields (i.e., only
--- src/utils/stack_allocator.cpp
+++ src/utils/stack_allocator.cpp
@@ -4,6 +4,111 @@
 /// Implementation of the stack allocator.
 
 namespace pbd {
+	stack_allocator::scoped_bookmark stack_allocator::scoped_bookmark::create(stack_allocator &alloc) {
+		scoped_bookmark result;
+		result._alloc = &alloc;
+		alloc.set_bookmark();
+		return result;
+	}
+
+
+	stack_allocator::_page_ref stack_allocator::_page_ref::to_new_page(void *ptr, std::size_t sz) {
+		_page_ref result = uninitialized;
+		result.memory = ptr;
+		result.current = ptr;
+		result.end = static_cast<std::byte*>(ptr) + sz;
+		return result;
+	}
+
+	void *stack_allocator::_page_ref::allocate(std::size_t size, std::size_t align) {
+		std::size_t sz = static_cast<std::byte*>(end) - static_cast<std::byte*>(current);
+		if (void *result = std::align(align, size, current, sz)) {
+			current = static_cast<std::byte*>(current) + size;
+			return result;
+		}
+		return nullptr;
+	}
+
+
+	stack_allocator::_page_header stack_allocator::_page_header::create(_page_ref prev, void (*free)(void*)) {
+		_page_header result = uninitialized;
+		result.previous = prev;
+		result.free_page = free;
+		return result;
+	}
+
+
+	stack_allocator::_bookmark stack_allocator::_bookmark::create(void *page, void *cur, _bookmark *prev) {
+		_bookmark result = uninitialized;
+		result.page = page;
+		result.current = cur;
+		result.previous = prev;
+		return result;
+	}
+
+
+	stack_allocator::~stack_allocator() {
+		assert(_top_bookmark == nullptr);
+		free_unused_pages();
+		while (_top_page) {
+			_page_ref next = _top_page.header->previous;
+			auto free_func = _top_page.header->free_page;
+			free_func(_top_page.memory);
+			_top_page = next;
+		}
+	}
+
+	void *stack_allocator::allocate(std::size_t size, std::size_t align) {
+		if (void *result = _top_page.allocate(size, align)) {
+			return result;
+		}
+		_take_page();
+		if (void *result = _top_page.allocate(size, align)) {
+			return result;
+		}
+		_return_page();
+		_top_page = _allocate_new_page(_top_page, page_size + size);
+		return _top_page.allocate(size, align);
+	}
+
+	void stack_allocator::pop_bookmark() {
+		assert(_top_bookmark);
+		_bookmark mark = *_top_bookmark;
+		_top_bookmark->~_bookmark();
+		_top_bookmark = mark.previous;
+		while (_top_page.memory != mark.page) {
+			_return_page();
+		}
+		_top_page.current = mark.current;
+	}
+
+	void stack_allocator::free_unused_pages() {
+		while (_free_pages) {
+			_page_ref next = _free_pages.header->previous;
+			auto free_func = _free_pages.header->free_page;
+			free_func(_free_pages.memory);
+			_free_pages = next;
+		}
+	}
+
+	void stack_allocator::_take_page() {
+		if (_free_pages) {
+			_page_ref page = _free_pages;
+			_free_pages = _free_pages.header->previous;
+			page.header->previous = _top_page;
+			_top_page = page;
+		} else {
+			_top_page = _allocate_new_page(_top_page);
+		}
+	}
+
+	void stack_allocator::_return_page() {
+		_page_ref new_top = _top_page.header->previous;
+		_top_page.reset(_page_header::create(_free_pages, _top_page.header->free_page));
+		_free_pages = _top_page;
+		_top_page = new_top;
+	}
+
 	stack_allocator &stack_allocator::for_this_thread() {
 		static thread_local stack_allocator _allocator;
 		return _allocator;

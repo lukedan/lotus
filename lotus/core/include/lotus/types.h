5d664a91c4283c425b91b51569877f5c98161f06
Xuanyi Zhou <zxy19980101@sina.cn>
2025-08-17 10:26:18 -0700

    add f32 and f64 definitions

--- lotus/core/include/lotus/types.h
+++ lotus/core/include/lotus/types.h
@@ -5,6 +5,9 @@
 
 #include <cstdint>
 #include <cstddef>
+#if __has_include(<stdfloat>)
+#	include <stdfloat>
+#endif
 
 namespace lotus {
 	/// Short definitions of all primitive type names.
@@ -19,5 +22,20 @@ namespace lotus {
 		using i16 = std::int16_t; ///< 16-bit signed integral type.
 		using i32 = std::int32_t; ///< 32-bit signed integral type.
 		using i64 = std::int64_t; ///< 64-bit signed integral type.
+
+		/// 32-bit floating point types.
+#ifdef __STDCPP_FLOAT32_T__
+		using f32 = std::float32_t;
+#else
+		using f32 = float;
+		static_assert(sizeof(f32) == sizeof(u32), "Expecting float to be 32 bits wide");
+#endif
+		/// 64-bit floating point types.
+#ifdef __STDCPP_FLOAT64_T__
+		using f64 = std::float64_t;
+#else
+		using f64 = double;
+		static_assert(sizeof(f64) == sizeof(u64), "Expecting double to be 64 bits wide");
+#endif
 	}
 }

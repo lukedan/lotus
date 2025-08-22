464797e0035789004633fb9cd784d95a6547b67e
Xuanyi Zhou <zxy19980101@sina.cn>
2025-08-21 23:43:31 -0700

    update the cosserat rod test to use physically accurate metrics

--- test/testbed/tests/cosserat_rod_test.h
+++ test/testbed/tests/cosserat_rod_test.h
@@ -11,7 +11,7 @@ public:
 	}
 
 	void timestep(scalar dt, u32 iterations) override {
-		//_solver_avbd.timestep(dt, iterations);
+		_solver_avbd.timestep(dt, iterations);
 		_solver_xpbd.timestep(dt, iterations);
 	}
 
@@ -45,13 +45,17 @@ public:
 		for (u32 x = 0; x < 5; ++x) {
 			for (u32 y = 0; y < 5; ++y) {
 				const vec3 start(0.01f * x, 0.01f * y, 0.0f);
-				const vec3 end = start + vec3(0.0f, 0.0f, 0.2f);
+				const vec3 end = start + vec3(0.0f, 0.0f, _length_m);
 
-				const vec3 pos_avbd(-0.5f, 0.0f, 0.0f);
-				_create_straight_rod_avbd(start + pos_avbd, end + pos_avbd, _segments, _mass, _k_ss, _k_bt);
+				const vec3 pos_avbd(0.0f, 0.0f, 0.0f);
+				_create_straight_rod_avbd(
+					start + pos_avbd, end + pos_avbd, _segments, _density_kg_m3, _diameter_m, _k_ss, _k_bt
+				);
 
 				const vec3 pos_xpbd(0.5f, 0.0f, 0.0f);
-				_create_straight_rod_xpbd(start + pos_xpbd, end + pos_xpbd, _segments, _mass, _k_ss, _k_bt);
+				_create_straight_rod_xpbd(
+					start + pos_xpbd, end + pos_xpbd, _segments, _density_kg_m3, _diameter_m, _k_ss, _k_bt
+				);
 			}
 		}
 	}
@@ -60,9 +64,11 @@ public:
 		const u32 min_segments = 2;
 		const u32 max_segments = 100;
 		ImGui::SliderScalar("Num Segments", ImGuiDataType_U32, &_segments, &min_segments, &max_segments);
-		ImGui::SliderFloat("Mass", &_mass, 0.0f, 1.0f);
-		ImGui::SliderFloat("Stretching-Shearing Compliance", &_k_ss, 0.0f, 10000.0f, "%.3f", ImGuiSliderFlags_Logarithmic);
-		ImGui::SliderFloat("Bending Compliance", &_k_bt, 0.0f, 10000.0f, "%.3f", ImGuiSliderFlags_Logarithmic);
+		ImGui::SliderFloat("Density kg/m3", &_density_kg_m3, 100.0f, 10000.0f);
+		ImGui::SliderFloat("Length", &_length_m, 0.01f, 1.0f);
+		ImGui::SliderFloat("Diameter m", &_diameter_m, 0.0001f, 0.1f, "%.4f", ImGuiSliderFlags_Logarithmic);
+		ImGui::SliderFloat("Stretching-Shearing Stiffness", &_k_ss, 0.0f, 10000.0f, "%.5f", ImGuiSliderFlags_Logarithmic);
+		ImGui::SliderFloat("Bending Stiffness", &_k_bt, 0.0f, 10000.0f, "%.5f", ImGuiSliderFlags_Logarithmic);
 
 		test::gui();
 	}
@@ -78,16 +84,22 @@ private:
 	debug_render _render;
 
 	u32 _segments = 10;
-	scalar _mass = 0.1f;
+	scalar _density_kg_m3 = 1000.0f;
+	scalar _length_m = 0.2f; // 20cm
+	scalar _diameter_m = 0.05f; // 5cm
 	scalar _k_ss = 1.0f;
 	scalar _k_bt = 1.0f;
 
 	template <typename Solver, typename BendCallback, typename StretchCallback> static void _create_straight_rod(
 		Solver &solver, BendCallback &&bend_cb, StretchCallback &&stretch_cb,
-		vec3 start, vec3 end, u32 num_parts, scalar total_mass
+		vec3 start, vec3 end, u32 num_parts, scalar density, scalar diameter
 	) {
+		const float volume = 0.25f * static_cast<f32>(lotus::constants::pi) * diameter * diameter * (end - start).norm();
+		const float total_mass = volume * density;
+
 		const vec3 part_offset = (end - start) / (num_parts - 1);
 		const scalar inv_part_mass = static_cast<scalar>(num_parts) / total_mass;
+		const scalar inv_inertia_mass = 8.0f * inv_part_mass / (diameter * diameter);
 
 		// add particles
 		const auto first_part = static_cast<u32>(solver.particles.size());
@@ -108,7 +120,7 @@ private:
 			lotus::physics::orientation &ori = solver.orientations.emplace_back(lotus::uninitialized);
 			ori.state            = lotus::physics::orientation_state::stationary_at(all_ori);
 			ori.prev_orientation = ori.state.orientation;
-			ori.inv_inertia      = i < 2 ? 0.0f : 10.0f; // TODO
+			ori.inv_inertia      = i < 2 ? 0.0f : inv_inertia_mass;
 		}
 
 		// set up bending-twisting constraints
@@ -128,7 +140,9 @@ private:
 		}
 	}
 
-	void _create_straight_rod_xpbd(vec3 start, vec3 end, u32 num_parts, scalar total_mass, scalar k_ss, scalar k_bt) {
+	void _create_straight_rod_xpbd(
+		vec3 start, vec3 end, u32 num_parts, scalar density, scalar diameter, scalar k_ss, scalar k_bt
+	) {
 		_create_straight_rod(
 			_solver_xpbd,
 			[&](u32 o1, u32 o2, uquats initial_bend) {
@@ -151,10 +165,11 @@ private:
 			start,
 			end,
 			num_parts,
-			total_mass
+			density,
+			diameter
 		);
 	}
-	void _create_straight_rod_avbd(vec3 start, vec3 end, u32 num_parts, scalar total_mass, scalar k_ss, scalar k_bt) {
+	void _create_straight_rod_avbd(vec3 start, vec3 end, u32 num_parts, scalar density, scalar diameter, scalar k_ss, scalar k_bt) {
 		_create_straight_rod(
 			_solver_avbd,
 			[&](u32 o1, u32 o2, uquats init_bend) {
@@ -177,7 +192,8 @@ private:
 			start,
 			end,
 			num_parts,
-			total_mass
+			density,
+			diameter
 		);
 	}
 };
